acf6bfe708683b4bb3890bca7d65eb45580b5a7f
jedychen <jedy829@gmail.com>
2021-03-11 23:17:20 +0800

    [Feature] Add Image Picker and OpenCV Integration
    - Add image picker
    - Change UI to display image
    - Add sample images
    - Integrate OpenCV with the official tutorial: https://docs.microsoft.com/en-us/windows/uwp/audio-video-camera/process-software-bitmaps-with-opencv

---
+++ FaceRecognitionUWP/ImageHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FaceRecognitionUWP
+{
+    public class ImageHelper
+    {
+        public static async Task<SoftwareBitmap> GetImageAsync(IRandomAccessStream stream)
+        {
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            return await decoder.GetSoftwareBitmapAsync();
+        }
+        public static async Task<StorageFile> PickerImageAsync()
+        {
+            var imagePicker = new FileOpenPicker()
+            {
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+
+            imagePicker.ViewMode = PickerViewMode.List;
+            imagePicker.FileTypeFilter.Add(".jpg");
+            imagePicker.FileTypeFilter.Add(".png");
+            imagePicker.FileTypeFilter.Add(".bmp");
+            imagePicker.FileTypeFilter.Add(".jpeg");
+            var file = await imagePicker.PickSingleFileAsync();
+            return file;
+        }
+
+/*        public static async Task<SoftwareBitmap> ResizeImage(SoftwareBitmapSource imageSource, int maxWidth, int maxHeight)
+        {
+            BitmapImage sourceImage = new BitmapImage();
+            sourceImage.(imageSource);
+            var origHeight = sourceImage.PixelHeight;
+            var origWidth = sourceImage.PixelWidth;
+            var ratioX = maxWidth / (float)origWidth;
+            var ratioY = maxHeight / (float)origHeight;
+            var ratio = Math.Min(ratioX, ratioY);
+            var newHeight = (int)(origHeight * ratio);
+            var newWidth = (int)(origWidth * ratio);
+
+            sourceImage.DecodePixelWidth = newWidth;
+            sourceImage.DecodePixelHeight = newHeight;
+
+            return sourceImage;
+        }*/
+    }
+}
--- FaceRecognitionUWP/MainPage.xaml.cs
+++ FaceRecognitionUWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCVBridge;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Windows.Foundation;
@@ -6,6 +7,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Storage;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
 #if USE_WINML_NUGET
 using Microsoft.AI.MachineLearning;
 #else
@@ -26,11 +28,16 @@ namespace FaceRecognitionUWP
         private Helper helper = new Helper();
         RenderTargetBitmap renderBitmap = new RenderTargetBitmap();
 
+
+        private RfbModel rfbModelGen;
+        private RfbInput rfbInput = new RfbInput();
+        private RfbOutput rfbOutput;
+
         public MainPage()
         {
             this.InitializeComponent();
 
-            // Set supported inking device types.
+            /*// Set supported inking device types.
             inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen | Windows.UI.Core.CoreInputDeviceTypes.Touch;
             inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(
                 new Windows.UI.Input.Inking.InkDrawingAttributes()
@@ -40,8 +47,8 @@ namespace FaceRecognitionUWP
                     IgnorePressure = true,
                     IgnoreTilt = true,
                 }
-            );
-            LoadModelAsync();
+            );*/
+            LoadFaceModelAsync();
             }
 
             private async Task LoadModelAsync()
@@ -51,7 +58,60 @@ namespace FaceRecognitionUWP
                 modelGen = await mnistModel.CreateFromStreamAsync(modelFile as IRandomAccessStreamReference);
             }
 
+            private async Task LoadFaceModelAsync()
+            {
+                //Load a machine learning model
+                StorageFile modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/version-RFB-320.onnx"));
+                modelGen = await mnistModel.CreateFromStreamAsync(modelFile as IRandomAccessStreamReference);
+            }
+
+
+            private async void selectButton_Click(object sender, RoutedEventArgs e)
+            {
+                var file = await ImageHelper.PickerImageAsync();
+                if (file != null)
+                {
+                    using (var fs = await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
+                    {
+
+                        using (var tempStream = fs.CloneStream())
+                        {
+                            SoftwareBitmap softImg;
+                            softImg = await ImageHelper.GetImageAsync(tempStream);
+
+                        SoftwareBitmap inputBitmap = SoftwareBitmap.Convert(softImg, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore);
+                        SoftwareBitmap outputBitmap = new SoftwareBitmap(softImg.BitmapPixelFormat, softImg.PixelWidth, softImg.PixelHeight, BitmapAlphaMode.Premultiplied);
+
+                        var helper = new OpenCVBridge.OpenCVHelper();
+                        helper.Blur(inputBitmap, outputBitmap);
+                        var img = new SoftwareBitmapSource();
+                            await img.SetBitmapAsync(outputBitmap);
+                            inputImage.Source = img;
+                        }
+                    }
+                }
+            }
+
             private async void recognizeButton_Click(object sender, RoutedEventArgs e)
+            {
+                
+                /*rfbInput.input = ImageFeatureValue.CreateFromVideoFrame(vf);
+
+                //Evaluate the model
+                mnistOutput = await modelGen.EvaluateAsync(mnistInput);
+
+                //Convert output to datatype
+                IReadOnlyList<float> vectorImage = mnistOutput.Plus214_Output_0.GetAsVectorView();
+                IList<float> imageList = vectorImage.ToList();
+
+                //LINQ query to check for highest probability digit
+                var maxIndex = imageList.IndexOf(imageList.Max());
+
+                //Display the results
+                numberLabel.Text = maxIndex.ToString();*/
+            }
+
+            async void recognizeButton2_Click()
             {
                 //Bind model input with contents from InkCanvas
                 VideoFrame vf = await helper.GetHandWrittenImage(inkGrid);
@@ -71,10 +131,12 @@ namespace FaceRecognitionUWP
                 numberLabel.Text = maxIndex.ToString();
             }
 
+            
+
             private void clearButton_Click(object sender, RoutedEventArgs e)
             {
-                inkCanvas.InkPresenter.StrokeContainer.Clear();
-                numberLabel.Text = "";
+                // inkCanvas.InkPresenter.StrokeContainer.Clear();
+                // numberLabel.Text = "";
             }
     }
 }
